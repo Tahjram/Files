b282cc18399612c5fc7f5b403a572eee17075d64
Tahjram <tahj.ramudith@my.uwi.edu>
2020-01-15 14:55:49 -0400

    Read Files

--- Files/Source.cpp
+++ Files/Source.cpp
@@ -1,1 +1,23 @@
 	//My Files
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+	std::ifstream input_file;
+	input_file.open("commaSeparatedValues.txt");
+	if (input_file) {
+		std::string mytext = "";
+		while (getline(input_file, mytext,',')) {
+			cout << mytext << endl;
+		}
+	}else {
+		cout << "That file does not exist" << endl;
+	}
+	input_file.close();
+	return 0;
+}
